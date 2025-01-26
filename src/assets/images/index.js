b9afb2890de748133ae93eb465a734ed38fbbe62
KhanhDo2604 <20521442@gm.uit.edu.vn>
2025-01-26 13:10:09 +1300

    complete Footer

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,5 +1,8 @@
 const images = {
     logo: require('./logo.svg'),
+    facebook: require('./facebook.svg'),
+    instagram: require('./instagram.svg'),
+    mail: require('./mail.svg'),
 };
 
 export default images;
--- src/components/Layout/DefaultLayout/Footer/index.jsx
+++ src/components/Layout/DefaultLayout/Footer/index.jsx
@@ -1,5 +1,57 @@
+import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
+import IconButton from '@material/react-icon-button';
+
+import styles from './Footer.module.scss';
+import config from '../../../../config';
+import images from '../../../../assets/images';
+
+const cx = classNames.bind(styles);
+
 function Footer() {
-    return <div></div>;
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('upper')}>
+                <div className={cx('left-inner')}>
+                    <span>Our platform is trusted by million & features best updated movies all around the world.</span>
+                </div>
+                <div className={cx('right-inner')}>
+                    <div className={cx('nav')}>
+                        <Link to={config.routes.home}>Home</Link>
+                        <span> / </span>
+                        <Link to={config.routes.home}>Discover</Link>
+                        <span> / </span>
+                        <Link to={config.routes.home}>Influence</Link>
+                        <span> / </span>
+                        <Link to={config.routes.home}>Release</Link>
+                    </div>
+                    <div className={cx('social-buttons')}>
+                        <IconButton>
+                            <img src={images.facebook} alt="facebook" width={28} />
+                        </IconButton>
+
+                        <IconButton>
+                            <img src={images.instagram} alt="instagram" width={28} />
+                        </IconButton>
+
+                        <IconButton>
+                            <img src={images.mail} alt="mail" width={28} />
+                        </IconButton>
+                    </div>
+                </div>
+            </div>
+            <div className={cx('lower')}>
+                <div className={cx('terms-buttons')}>
+                    <button>Privacy policy</button>
+                    <button>Term of service</button>
+                    <button>Language</button>
+                </div>
+                <div className={cx('copy-right')}>
+                    <span>@2025</span>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Footer;
